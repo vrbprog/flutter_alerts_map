002cf8f786ab20f0103a9466905d71c11c49a4f7
Roman <vrbox@i.ua>
2026-06-15 11:49:53 +0300

    feat: create Home screen design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_alerts_map/widgets/home_screen_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Center(child: Text('Ukrainian Alerts Map')));
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFF1DC0F4), Color(0xFFF2F67C)],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                spacing: 8,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  HomeScreenCard(type: HomeCardType.map, onTap: () => ()),
+                  HomeScreenCard(type: HomeCardType.region, onTap: () => ()),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
+}
+
+enum HomeCardType {
+  map(cardName: 'Alerts Map', iconName: 'assets/icons/icon_map.png'),
+  region(cardName: 'Region Alerts', iconName: 'assets/icons/icon_city.png');
+
+  const HomeCardType({required this.cardName, required this.iconName});
+
+  final String cardName;
+  final String iconName;
 }
---
+++ lib/widgets/home_screen_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_alerts_map/screens/home_screen.dart';
+
+class HomeScreenCard extends StatelessWidget {
+  const HomeScreenCard({required this.type, required this.onTap, super.key});
+
+  final HomeCardType type;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      child: InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 18.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(type.iconName, width: 22, height: 22),
+              const SizedBox(width: 12),
+              Text(
+                type.cardName,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
